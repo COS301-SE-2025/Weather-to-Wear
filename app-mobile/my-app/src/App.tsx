a9e763a50ce06e7761847b8997aa0af16fd9d2b2
TaylorSergel <u22517406@tuks.co.za>
2025-05-23 17:05:53 +0200

    Fix: Updated App.tsx layout and routing

--- app-mobile/my-app/src/App.tsx
+++ app-mobile/my-app/src/App.tsx
@@ -6,7 +6,7 @@ import HomePage from "./pages/HomePage";
 import ClosetPage from "./pages/ClosetPage";
 import AddPage from "./pages/UnderConstruction";
 import CalendarPage from "./pages/UnderConstruction";
-import FeedPage from "./pages/UnderConstruction";
+import FeedPage from "./pages/FeedPage";
 import ProfilePage from "./pages/UnderConstruction";
 import CreateAnOutfit from "./pages/UnderConstruction";
 import Post from "./pages/Post";
@@ -19,89 +19,30 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Redirect to login by default */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
         {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        {/* Main App Routes with NavBar */}
+        {/* Main App Routes */}
         <Route
-          path="/"
+          path="/*"
           element={
             <>
               <NavBar />
-              <HomePage />
-            </>
-          }
-        />
-        <Route
-          path="/closet"
-          element={
-            <>
-              <NavBar />
-              <ClosetPage />
-            </>
-          }
-        />
-        <Route
-          path="/add"
-          element={
-            <>
-              <NavBar />
-              <AddPage />
-            </>
-          }
-        />
-        <Route
-          path="/calendar"
-          element={
-            <>
-              <NavBar />
-              <CalendarPage />
-            </>
-          }
-        />
-        <Route
-          path="/feed"
-          element={
-            <>
-              <NavBar />
-              <FeedPage />
-            </>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <>
-              <NavBar />
-              <ProfilePage />
-            </>
-          }
-        />
-        <Route
-          path="/create-outfit"
-          element={
-            <>
-              <NavBar />
-              <CreateAnOutfit />
-            </>
-          }
-        />
-        <Route
-          path="/post-to-feed"
-          element={
-            <>
-              <NavBar />
-              <Post />
-            </>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <>
-              <NavBar />
-              <UnderConstruction />
+              <Routes>
+                <Route path="dashboard" element={<HomePage />} />
+                <Route path="closet" element={<ClosetPage />} />
+                <Route path="add" element={<AddPage />} />
+                <Route path="calendar" element={<CalendarPage />} />
+                <Route path="feed" element={<FeedPage />} />
+                <Route path="profile" element={<ProfilePage />} />
+                <Route path="create-outfit" element={<CreateAnOutfit />} />
+                <Route path="post-to-feed" element={<Post />} />
+                <Route path="*" element={<UnderConstruction />} />
+              </Routes>
             </>
           }
         />
