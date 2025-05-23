84292374c0187db16992524d1759f22aff94c13a
Beems <u22567072@tuks.co.za>
2025-05-23 14:07:40 +0200

    Trying to get rid of the CORS porblem that keeps coming up, also created a route to imaageUpload.ts

--- app-backend/src/app.ts
+++ app-backend/src/app.ts
@@ -1,13 +1,35 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './modules/auth/auth.routes';
+import path from 'path';
+import imageUploads from './modules/closet/imageUpload';
 
 dotenv.config();
 
 const app = express();
+
+// full cors config:
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // include both forms
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
+
+// handle preflight requests
+// app.options('*', cors());
+
+
+
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Routes
+console.log('Routes initialized');
 app.use('/api/auth', authRoutes);
+app.use('/api/closet', imageUploads); 
 
 export default app;
