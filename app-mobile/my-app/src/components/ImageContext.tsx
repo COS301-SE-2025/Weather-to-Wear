d6fffa3e289d88d870c2c5b5a942dd24ec394a5f
TaylorSergel <u22517406@tuks.co.za>
2025-05-25 13:55:18 +0200

    Created a new component to handle images when a user uploads or takes one to add a new item to closet

---
+++ app-mobile/my-app/src/components/ImageContext.tsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+interface ImageContextType {
+  image: string | null;
+  setImage: (image: string | null) => void;
+}
+
+const ImageContext = createContext<ImageContextType | undefined>(undefined);
+
+export const useImage = () => {
+  const context = useContext(ImageContext);
+  if (!context) throw new Error("useImage must be used within ImageProvider");
+  return context;
+};
+
+export const ImageProvider = ({ children }: { children: ReactNode }) => {
+  const [image, setImage] = useState<string | null>(null);
+  return (
+    <ImageContext.Provider value={{ image, setImage }}>
+      {children}
+    </ImageContext.Provider>
+  );
+};
