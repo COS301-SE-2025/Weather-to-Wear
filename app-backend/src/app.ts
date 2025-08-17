e4063e27d5c842e3f2cb91bc501a310f1f73b85e
TaylorSergel <u22517406@tuks.co.za>
2025-08-17 14:20:38 +0200

    updated the file to match bemo's fixes

--- app-backend/src/app.ts
+++ app-backend/src/app.ts
@@ -11,10 +11,12 @@ import eventsRoutes from './modules/events/events.route';
 import outfitRoutes from './modules/outfit/outfit.routes';
 import socialRoutes from './modules/social/social.route';
 import packingRoutes from './modules/packing/packing.route';
+
 dotenv.config();
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 const UPLOADS_DIR = path.join(__dirname, '..', 'uploads');
@@ -24,9 +26,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/weather', weatherRoutes);
 app.use('/api/closet', closetRoutes);
 app.use('/api/preferences', userPrefRoutes);
-app.use('/api/events', eventsRoutes);
-app.use('/api/events/packing', packingRoutes);
+app.use('/api/events', eventsRoutes); 
 app.use('/api/outfits', outfitRoutes);
 app.use('/api/social', socialRoutes);
+app.use('/api/packing', packingRoutes);
+
 
 export default app;
